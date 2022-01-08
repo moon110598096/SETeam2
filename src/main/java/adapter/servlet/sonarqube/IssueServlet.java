af3b8e267b8c03d0285df7b4a4cc7f31cb227b44
110598055 <110598055@ntut.org.tw>
2022-01-08 19:20:13 +0800

    [Backend] add issueServlet and  issueServletTest

---
+++ src/main/java/adapter/servlet/sonarqube/IssueServlet.java
@@ -0,0 +1,65 @@
+package adapter.servlet.sonarqube;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import usecase.SonarQubeRepositoryAccessor;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class IssueServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JSONObject requestBody = new JSONObject(request.getReader().readLine());
+        String componentKeys = requestBody.getString("componentKeys");
+
+        JSONObject issue_messageInfo = getIssueMessageJSONObject(componentKeys);
+
+        request.setAttribute("issue_message_info", issue_messageInfo);
+
+        response.setContentType("text/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println(issue_messageInfo);
+        System.out.println(issue_messageInfo);
+        out.flush();
+        out.close();
+    }
+
+    private JSONObject getIssueMessageJSONObject(String componentKeys) throws IOException {
+        String apiUrl = "http://140.124.181.10:9000/api/issues/search?componentKeys=" + componentKeys;
+        SonarQubeRepositoryAccessor accessor = new SonarQubeRepositoryAccessor();
+        JSONArray jsonArray = accessor.httpGet(apiUrl);
+        JSONArray issueJSONArray = jsonArray.getJSONObject(0).getJSONArray("issues");
+
+        JSONObject issue_messageResultJSONObject = new JSONObject();
+        JSONArray bugJSONArray = new JSONArray();
+        JSONArray code_smellJSONArray = new JSONArray();
+        JSONArray vulnerabilityJSONArray = new JSONArray();
+
+        for(Object statsObject : issueJSONArray){
+            JSONObject statsJsonObject = (JSONObject) statsObject;
+            JSONObject tempJSONObject = new JSONObject();
+            if(statsJsonObject.getString("type").equals("BUG")){
+                tempJSONObject.put("message", statsJsonObject.getString("message"));
+                bugJSONArray.put(tempJSONObject);
+            }else if(statsJsonObject.getString("type").equals("CODE_SMELL")){
+                tempJSONObject.put("message", statsJsonObject.getString("message"));
+                code_smellJSONArray.put(tempJSONObject);
+            }else if(statsJsonObject.getString("type").equals("VULNERABILITY")){
+                tempJSONObject.put("message", statsJsonObject.getString("message"));
+                vulnerabilityJSONArray.put(tempJSONObject);
+            }
+        }
+        issue_messageResultJSONObject.put("bugs",bugJSONArray);
+        issue_messageResultJSONObject.put("code_smells",code_smellJSONArray);
+        issue_messageResultJSONObject.put("vulnerabilities",vulnerabilityJSONArray);
+
+        return issue_messageResultJSONObject;
+    }
+
+}
---
+++ src/test/java/adapter/servlet/sonarqube/SonarqubeIssueServletTest.java
@@ -0,0 +1,38 @@
+package adapter.servlet.sonarqube;
+
+import adapter.servlet.sonarqube.IssueServlet;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class SonarqubeIssueServletTest {
+    private MockHttpServletRequest request;
+    private HttpServletResponse response;
+    private IssueServlet issueServlet;
+
+    @Before
+    public void setUp() throws IOException {
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        issueServlet = new IssueServlet();
+        request.addHeader("Content-Type","text/json");
+        request.setContent("{componentKeys:GitRepositoryAnalysisSystem}".getBytes(StandardCharsets.UTF_8));
+
+    }
+    @Test
+    public void GetIssueMessage() throws IOException {
+        issueServlet.doPost(request, response);
+        JSONObject jsonObject = (JSONObject) request.getAttribute("issue_message_info");
+        System.out.println(jsonObject);
+        Assert.assertNotNull(jsonObject.getJSONArray("bugs"));
+        Assert.assertNotNull(jsonObject.getJSONArray("code_smells"));
+        Assert.assertNotNull(jsonObject.getJSONArray("vulnerabilities"));
+    }
+}
