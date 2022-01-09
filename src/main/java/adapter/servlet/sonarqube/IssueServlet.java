4c3cf9b36120bd77fcf35ee397defcf4e9922f5d
110598055 <110598055@ntut.org.tw>
2022-01-09 17:47:03 +0800

    [Backend] modify IssueServlet

--- src/main/java/adapter/servlet/sonarqube/IssueServlet.java
+++ src/main/java/adapter/servlet/sonarqube/IssueServlet.java
@@ -31,7 +31,7 @@ public class IssueServlet extends HttpServlet{
     }
 
     private JSONObject getIssueMessageJSONObject(String componentKeys) throws IOException {
-        String apiUrl = "http://140.124.181.10:9000/api/issues/search?componentKeys=" + componentKeys;
+        String apiUrl = "http://140.124.181.10:9000/api/issues/search?componentKeys=" + componentKeys + "&ps=500&types=CODE_SMELL,BUG,VULNERABILITY&statuses=OPEN";
         SonarQubeRepositoryAccessor accessor = new SonarQubeRepositoryAccessor();
         JSONArray jsonArray = accessor.httpGet(apiUrl);
         JSONArray issueJSONArray = jsonArray.getJSONObject(0).getJSONArray("issues");
@@ -40,21 +40,32 @@ public class IssueServlet extends HttpServlet{
         JSONArray bugJSONArray = new JSONArray();
         JSONArray code_smellJSONArray = new JSONArray();
         JSONArray vulnerabilityJSONArray = new JSONArray();
-
+        int count = 0;
+        int countBug =0;
+        int countB =0;
+        int countV = 0;
         for(Object statsObject : issueJSONArray){
             JSONObject statsJsonObject = (JSONObject) statsObject;
             JSONObject tempJSONObject = new JSONObject();
+            count++;
             if(statsJsonObject.getString("type").equals("BUG")){
                 tempJSONObject.put("message", statsJsonObject.getString("message"));
                 bugJSONArray.put(tempJSONObject);
+                countBug ++;
             }else if(statsJsonObject.getString("type").equals("CODE_SMELL")){
                 tempJSONObject.put("message", statsJsonObject.getString("message"));
                 code_smellJSONArray.put(tempJSONObject);
+                countB ++;
             }else if(statsJsonObject.getString("type").equals("VULNERABILITY")){
                 tempJSONObject.put("message", statsJsonObject.getString("message"));
                 vulnerabilityJSONArray.put(tempJSONObject);
+                countV ++;
             }
         }
+        System.out.println(count);
+        System.out.println(countBug);
+        System.out.println(countB);
+        System.out.println(countV);
         issue_messageResultJSONObject.put("bugs",bugJSONArray);
         issue_messageResultJSONObject.put("code_smells",code_smellJSONArray);
         issue_messageResultJSONObject.put("vulnerabilities",vulnerabilityJSONArray);
